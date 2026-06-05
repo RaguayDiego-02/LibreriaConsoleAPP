addcb137589f061afe49e383647f681bc2308fc6
Joshua Suy <jsuy-2026083@kinal.edu.gt>
2026-06-05 10:44:46 -0600

    009 Se actualizo la clase principal, agregue un saludo

--- src/org/diegoraguay/system/Principal.java
+++ src/org/diegoraguay/system/Principal.java
@@ -3,8 +3,8 @@ package org.diegoraguay.system;
 public class Principal {
     
     public static void main(String[] args) {
-        System.out.println("Hola mundo"); 
-        
+        System.out.println("Hola mundo - Soy Joshua"); 
+        System.out.println("Estoy en kinal - 2026083"); 
     }
     
 }
