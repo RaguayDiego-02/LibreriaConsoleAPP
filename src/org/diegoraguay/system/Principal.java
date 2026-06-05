7f5646e6e2c5d590e9d367e2a4dd28f8a6cf2d41
levi salazar <lsalazar-2026039@kinal.edu.gt>
2026-06-05 10:44:46 -0600

    007 se modifico la clase principal.java, agrese mis datos

--- src/org/diegoraguay/system/Principal.java
+++ src/org/diegoraguay/system/Principal.java
@@ -3,8 +3,8 @@ package org.diegoraguay.system;
 public class Principal {
     
     public static void main(String[] args) {
-        System.out.println("Hola mundo"); 
-        
+        System.out.println("Hola mundo - soy levi"); 
+        System.out.println("Estoy en kinal - 2026039");       
     }
     
 }
