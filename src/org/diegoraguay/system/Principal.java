75e4d483fb11d159dd8823ed6d3003e5cb36239a
Kenneth Palencia <kpalencia-2023259@kinal.edu.gt>
2026-06-05 10:44:58 -0600

    008 - se actualizo a calse porincipal, agregue un saludo

--- src/org/diegoraguay/system/Principal.java
+++ src/org/diegoraguay/system/Principal.java
@@ -3,7 +3,8 @@ package org.diegoraguay.system;
 public class Principal {
     
     public static void main(String[] args) {
-        System.out.println("Hola mundo"); 
+        System.out.println("Hola mundo, soy Kenneth"); 
+        System.out.println("Estoy en kinal - 2023259");
         
     }
     
